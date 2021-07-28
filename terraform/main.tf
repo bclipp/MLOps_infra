196dec7c27772be1d7a2c92ce4c949d59a0cf77e
Brian Lipp <bclipp770@gmail.com>
2021-07-28 14:24:47 -0400

    adding new cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,9 +27,31 @@ data "databricks_spark_version" "latest_lts" {
   long_term_support = true
 }
 
-resource "databricks_cluster" "mlops_tiny" {
+resource "databricks_cluster" "tiny" {
   cluster_name            = "mlops_tiny"
   spark_version           = data.databricks_spark_version.latest_lts.id
+  node_type_id            = "m4.large"
+  driver_node_type_id     = "m4.large"
+  autotermination_minutes = 10
+  autoscale {
+    min_workers = 1
+    max_workers = 2
+  }
+  aws_attributes {
+        first_on_demand = 1
+        availability = "SPOT_WITH_FALLBACK"
+        zone_id = "us-west-2b"
+        instance_profile_arn = "arn:aws:iam::112437402463:instance-profile/databricks_instance_role_s3"
+        spot_bid_price_percent = 100
+        ebs_volume_type = "GENERAL_PURPOSE_SSD"
+        ebs_volume_count = 3
+        ebs_volume_size = 100
+    }
+}
+
+resource "databricks_cluster" "ml_tiny" {
+  cluster_name            = "mlops_tiny"
+  spark_version           = "8.4.x-cpu-ml-scala2.12"
   node_type_id            = "m4.large"
   driver_node_type_id     = "m4.large"
   autotermination_minutes = 10
